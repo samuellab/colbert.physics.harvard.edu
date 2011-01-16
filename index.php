e69c5aef4ed6416d413f619d349cc9b66201aee5
Andrew Leifer <andrew.leifer@gmail.com>
2011-01-16 12:14:52 -0500

    cleaned up frontpage

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$title="Vidoes";
+	$title="Optogenetic tool for neuroscience";
 	include("header.inc");
 ?>
 <!------- END OF TOP MATTER -->
@@ -7,14 +7,14 @@
 <div id="mainpanel" class ="span-16 append-1">
 <h2>An optogenetic technique for neuroscience that uses lasers to manipulate neurocircuits in moving animals</h2> 
 
-<p>CoLBeRT is a new technique for neuroscience developed by the <a href="http://worms.physics.harvard.edu"/>Samuel Lab</a> at Harvard University's <a href="http://cbs.fas.harvard.edu/">Center For Brain Science</a> that allows researchers to use lasers to remotely control the neural activity of an animal as it moves. The system has been used thus far on the microscopic nematode <em>C. elegans</em>. </p>
+<p>CoLBeRT is a new technique for neuroscience developed by the <a href="http://worms.physics.harvard.edu"/>Samuel Lab</a> at Harvard University's <a href="http://cbs.fas.harvard.edu/">Center For Brain Science</a> that allows researchers commandeer the nervous system of a moving animal without wires or electrodes. The system has been used thus far on the microscopic nematode <em>C. elegans</em>. </p>
 
 <img class="left" src="images/colbert_schematic.png" width="400px">
 
 <p>Scientists are using CoLBeRT to understand how a handful of neurons can work together in an animal to generate behavior. The nematode <em>C. elegans</em> is an ideal organism to study because it  has only 302 neurons and it is easy to modify genetically. For comparison, a human being has roughly 100 billion neurons. The CoLBeRT system uses nematodes that express an extra gene for a light-sensitive ion channel protein, such as Channelrhodopsin or Halorhodopsin. This extra protein is similar to the protein in a human's eye. When added to the worm's neurons, these proteins makes the worm's neurons sensitive to different colors of light. The CoLBeRT system tracks the worm as it moves and shines laser light on specific neurons as the worm is moving to stimulate or inhibit those neurons. </p>
 
 <p>
-In this way the worm can be induced to paralyze, lay eggs, back up, speed up or sense touch in different areas of its body. The Samuel Lab is using this tool to study different neurocircuits in the worm. For more information see [PUBLICATION HERE]. Videos of hte CoLBeRT system are here. All of the associated software and souce code for the CoLBeRT system is free to download. 
+In this way the worm can be induced to paralyze, lay eggs, back up, speed up or sense touch in different areas of its body. The Samuel Lab is using this tool to study different neurocircuits in the worm. <!-- Add in link to publication here --> Videos of the CoLBeRT system are <a href="videos.php">avaialable here</a>. All of the associated software and souce code for the CoLBeRT system is free to download. 
 </p>
 
 </div>
