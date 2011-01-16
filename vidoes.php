894f3c364b129fbd0282804ab6920a7c6613fd4a
Andrew Leifer <andrew.leifer@gmail.com>
2011-01-16 00:16:38 -0500

    added php support

---
+++ vidoes.php
@@ -0,0 +1,44 @@
+<?php
+	$title="An optogenetic technique to manipulate neurocircuits in moving animals";
+	include("header.inc");
+?>
+<div id="mainpanel" class ="span-16 append-1">
+<h2>An optogenetic technique for neuroscience that uses lasers to manipulate neurocircuits in moving animals</h2> 
+
+<p>CoLBeRT is a new technique for neuroscience developed by the <a href="http://worms.physics.harvard.edu"/>Samuel Lab</a> at Harvard University's <a href="http://cbs.fas.harvard.edu/">Center For Brain Science</a> that allows researchers to use lasers to remotely control the neural activity of an animal as it moves. The system has been used thus far on the microscopic nematode <em>C. elegans</em>. </p>
+<img class="left" src="images/colbert_schematic.png" width="400px">
+
+<p>Scientists are using CoLBeRT to understand how a handful of neurons can work together in an animal to generate behavior. The nematode <em>C. elegans</em> is an ideal organism to study because it  has only 302 neurons and it is easy to modify genetically. For comparison, a human being has roughly 100 billion neurons. The CoLBeRT system uses nematodes that express an extra gene for a light-sensitive ion channel protein, such as Channelrhodopsin or Halorhodopsin. This extra protein is similar to the protein in a human's eye. When added to the worm's neurons, these proteins makes the worm's neurons sensitive to different colors of light. The CoLBeRT system tracks the worm as it moves and shines laser light on specific neurons as the worm is moving to stimulate or inhibit those neurons. </p>
+
+<p>
+In this way the worm can be induced to paralyze, lay eggs, back up, speed up or sense touch in different areas of its body. The Samuel Lab is using this tool to study different neurocircuits in the worm. For more information see [PUBLICATION HERE]. Videos of hte CoLBeRT system are here. All of the associated software and souce code for the CoLBeRT system is free to download. 
+</p>
+
+</div>
+
+
+
+
+
+<div class="span-7 last">
+
+<div class="box">
+<h3> Stephen Colbert</h3>
+<a href="http://www.flickr.com/photos/mikebrowne/4365625233/lightbox/"><img class="right" alt="Stephen Colbert" src="images/colbert_mikeBrowne_small.jpg" ></a>
+The CoLBeRT project is dedicated to its namesake, <a href="http://colbertnation.com">Stephen Colbert</a>, who manipulates the neurocircuits of millions of Americans daily using only the light from their monitors. Photo (cc) <a href="http://www.flickr.com/photos/mikebrowne/">MikeBrowne</a>.
+</div>
+<div class="box">
+<h3> CoLBeRT in Action</h3>
+<iframe   src="http://player.vimeo.com/video/16933869?title=0&byline=0&portrait=0&color=ff9933" width="250" height="200" frameborder="0"></iframe>
+Blue light causes the worm to experience the sensation touch, and so it recoils.See <a href="videos.php">more vidoes</a>.
+
+</div>
+
+</div>
+
+
+
+<?php
+include("footer.inc");
+?>
+
