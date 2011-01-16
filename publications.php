130ba42ce407b62dd94f5eed4fe86302feb7c740
Andrew Leifer <andrew.leifer@gmail.com>
2011-01-16 15:15:12 -0500

    moved

--- publications.php
+++ publications.php
@@ -2,7 +2,7 @@
 	$title="Code";
 	include("header.inc");
 ?>
-<div id="mainpanel" class ="span-16 append-1">
+<div id="mainpanel" class ="span-2323 append-1">
 <h2>Journal Articles</h2>
 <ul>
 <li>Andrew M Leifer, Christopher Fang-Yen, Marc Gershow, Mark J Alkema, & Aravinthan D.T. Samuel, "Optogenetic manipulation of neural activity in freely moving <i>Caenorhabditis elegans</i>", <i>Nature Methods</i>, advanced online publication, 16 January 2011.<a href="media/leifer_et_al_2011.pdf">[ PDF ]</a></li>
@@ -11,7 +11,12 @@
 <h2>In the popular press</h2>
 <ul>
 <li>Laura Sanders, <a href="http://www.sciencenews.org/view/generic/id/68860/description/Making_a_worm_do_more_than_squirm#video" target="_blank">Making a worm do more than squirm</a> <i>Science News</i> 16 January 2011.</li>
+
+<li>David Castelvecchi, <a href="http://www.scientificamerican.com/article.cfm?id=single-worm-neurons">"Single Worm Neurons Remotely Controlled with Lasers"</a>, <i>Scientific American</i> 16 January 2011.</li>
+
 </ul>
+
+
 
 </div>
 <?php
