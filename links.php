6d39a266a42ac5140557db76eabe43da3a731dfb
Andrew Leifer <andrew.leifer@gmail.com>
2011-01-16 12:15:09 -0500

    links

---
+++ links.php
@@ -0,0 +1,37 @@
+<?php
+	$title="Videos";
+	include("header.inc");
+?>
+<div id="mainpanel" class ="span-16 append-1">
+
+<h2>CoLBeRT Team</h2>
+<ul>
+<li><a href="http://www.andrewleifer.com" target="_blank">Andrew Leifer</a></li>
+<li><a href="http://fangyenlab.seas.upenn.edu/" target="_blank">Chris Fang-Yen</a></li>
+<li><a href="http://worms.physics.harvard.edu/peeps.htm" target="_blank">Marc Gershow</a></li>
+<li><a href="http://www.umassmed.edu/neuroscience/faculty/alkema.cfm" target="_blank">Mark Alkema</a></li>
+<li><a href="http://worms.physics.harvard.edu" target="_blank">Aravi Samuel</a></li>
+</ul>
+
+
+<h2>Harvard</h2>
+<ul>
+<li><a href="http://physics.harvard.edu/" target="_blank">Department of Physics</a></li>
+<li><a href="http://www.fas.harvard.edu/~biophys/" target="_blank">Graduate Program in Biophysics</a></li>
+<li><a href="http://cbs.fas.harvard.edu/" target="_blank">Center for Brain Science</a></li>
+</ul>
+
+<h2>C. elegans resources</h2>
+
+<ul>
+<li><a href="http://www.wormatlas.org/" target="_blank">Worm Atlas</a></li>
+<li><a href="http://www.wormbook.org/" target="_blank">Worm Book</a></li>
+</ul>
+
+
+
+</div>
+<?php
+include("footer.inc");
+?>
+
