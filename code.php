12540d92180dc43119404ed5aeaa8d334bdc6b7e
Andrew Leifer <andrew.leifer@gmail.com>
2011-01-16 12:36:44 -0500

    code done

--- code.php
+++ code.php
@@ -8,8 +8,9 @@
 All of the software written for the CoLBeRT system is freely available under the open source GNU General Public License. Visit the <a href="http://github.com/samuellab" target="_blank">Samuel Lab page</a> at GitHub.
 </p>
 <h2>MindControl</h2>
+<a href="images/fullres/mindcontrol_screenshot_full.png"> <img class="image" src="images/mindcontrol_screenshot.png"></a>
 <p>
-The software to track a moving worm and generate illumination patterns targeted at specific neurons or muscle is called  MindControl. MindControl was written in C by Andrew Leifer using the open source <a href="http://opencv.willowgarage.com/wiki/" target="_blank">OpenCV</a> computer vision library. The source code for mindcontrol is avaiable <a href="http://github.com/samuellab/mindcontrol">through GitHub</a>. Mind control also runs in a simulation mode, so anyone with a Windows computer can download the <a href="https://github.com/downloads/samuellab/mindcontrol/MindControlDEMO.zip">Mindcontrol Demo</a> package and run it for themselves. 
+The software to track a moving worm and generate illumination patterns targeted at specific neurons or muscle is called  MindControl. MindControl was written in C by Andrew Leifer using the open source <a href="http://opencv.willowgarage.com/wiki/" target="_blank">OpenCV</a> computer vision library. The source code for mindcontrol is avaiable <a href="http://github.com/samuellab/mindcontrol">through GitHub</a>. Mind control also runs in a simulation mode, so anyone with a Windows computer can download the <a href="https://github.com/downloads/samuellab/mindcontrol/MindControlDEMO.zip">Mindcontrol Demo</a> package and run the software using included video of a swimming worm. 
 </p>
 
 
