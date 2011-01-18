507047ce12c44c558bc6c47295500b0a101e70df
Andrew <leifer@fas.harvard.edu>
2011-01-18 16:16:19 -0500

    press section

--- publications.php
+++ publications.php
@@ -2,27 +2,14 @@
 	$title="Code";
 	include("header.inc");
 ?>
-<div id="mainpanel" class ="span-2323 append-1">
+<div id="mainpanel" class ="span-23 append-1">
 <h2>Journal Articles</h2>
 <ul>
 <li>Andrew M Leifer, Christopher Fang-Yen, Marc Gershow, Mark J Alkema, & Aravinthan D.T. Samuel, "<a href="http://www.nature.com/doifinder/10.1038/nmeth.1554" target="_blank">Optogenetic manipulation of neural activity in freely moving <i>Caenorhabditis elegans</i></a>", <i>Nature Methods</i>, advanced online publication, 16 January 2011. [<a href="media/leifer_et_al_2011.pdf"> PDF </a>]</li>
 </ul>
 
-<h2>In the popular press</h2>
-<ul>
-<li>Laura Sanders, <a href="http://www.sciencenews.org/view/generic/id/68860/description/Making_a_worm_do_more_than_squirm#video" target="_blank">Making a worm do more than squirm</a> <i>Science News</i> 16 January 2011.</li>
 
-<li>David Castelvecchi, <a href="http://www.scientificamerican.com/article.cfm?id=single-worm-neurons">"Single Worm Neurons Remotely Controlled with Lasers"</a>, <i>Scientific American</i> 16 January 2011.</li>
 
-</ul>
-
-<h2>Around the web</h2>
-<ul>
-<li>Sam Sheffer, <a href="http://www.engadget.com/2011/01/18/harvard-university-controls-worm-with-laser-we-wait-for-choreog/" target="_break">Harvard University controls worm wtih laser, we wait for choreagraphed dance moves (video)</a>, <i>Engadget</i></li>
-<li>Andrew Moseman, <a href="http://blogs.discovermagazine.com/80beats/2011/01/18/zapping-worm-brains-with-lasers-reveals-the-duty-of-each-neuron/" target=
-"_blank">Zapping Worm Brains With Lasers Reveals the Duty of Each Neuron</a>, <i>Discover Magazine</i>.</li>
-<li>Stephen Sparkman, <a href="http://www.newsy.com/videos/remote-for-worms-named-after-stephen-colbert" target="_blank">Remote for Worms Named After Stephen Colbert [ VIDEO ]</a>, <i>Newsy</i>. 
-</ul>
 </div>
 <?php
 include("footer.inc");
